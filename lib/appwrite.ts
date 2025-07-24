5f2ed500821e1179193d91253d18744713be1733
Allan Kiplagat <allankiplgat@gmail.com>
2025-07-24 13:47:28 +0300

    Update : fix database

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -5,13 +5,14 @@ export const appwriteConfig = {
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
     platform: "com.jsm.foodordering",
-    databaseId: '68629ae60038a7c61fe4',
+    databaseId: '68790f05003864a62e14',
     bucketId: '68643e170015edaa95d7',
     userCollectionId: '68629b0a003d27acb18f',
     categoriesCollectionId: '68643a390017b239fa0f',
     menuCollectionId: '68643ad80027ddb96920',
     customizationsCollectionId: '68643c0300297e5abc95',
     menuCustomizationsCollectionId: '68643cd8003580ecdd8f'
+    //68790f05003864a62e14
 }
 
 export const client = new Client();
